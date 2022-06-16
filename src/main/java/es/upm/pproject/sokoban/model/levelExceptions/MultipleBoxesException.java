e9e3abdd71c5ca4c5a6b18c2a9706adc0d34b745
LE quang, VINH <vinh.lequang@alumnos.upm.es>
2022-06-16 22:56:33 +0200

    Delete MultipleBoxesException.java

--- src/main/java/es/upm/pproject/sokoban/model/levelExceptions/MultipleBoxesException.java
+++
@@ -1,7 +0,0 @@
-package es.upm.pproject.sokoban.model.levelExceptions;
-
-public class MultipleBoxesException extends Exception {
-    public MultipleBoxesException(String message) {
-        super(message);
-    }
-}
